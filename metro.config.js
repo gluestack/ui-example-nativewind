873c26606da40c43bc8fc48e914dbf2457559359
Rajat Chaudhary <rajatchaudhary@Rajats-MacBook-Air.local>
2024-04-18 19:09:37 +0530

    fix: build issue

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,8 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname, {
+    isCSSEnabled: true
+});
 
 module.exports = withNativeWind(config, { input: './global.css' });
